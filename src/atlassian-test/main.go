99ce3224d4eca9da7529acb11d529debdc2209e6
Anton Denisenko <secorjeretweaz@yandex.ru>
2018-09-26 14:59:07 +0300

    Update main.go

--- src/atlassian-test/main.go
+++ src/atlassian-test/main.go
@@ -9,13 +9,18 @@ import (
 )
 
 func main() {
-	// Setting log level, default is Warning
+	
+		// !Warning: Setting log level, default.
+	
 	loglevel, err := log.ParseLevel(getEnv("LOGLEVEL", "warn"))
 	if err == nil {
 		log.SetLevel(loglevel)
-	}
+}
+	
 	log.Printf("Log level: %s", log.GetLevel())
-	// Starting HTTP server with one handler
+	
+		// Starting HTTP server with one handler
+	
 	http.HandleFunc("/filter", filterHandler)
 	log.Fatal(http.ListenAndServe(getEnv("LISTEN", ":8080"), nil))
 }
