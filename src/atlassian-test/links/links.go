918113401ffb5be2b569cd46ff636b21b74fbc3d
Anton Denisenko <secorjeretweaz@yandex.ru>
2018-09-29 09:30:13 +0300

    Update links.go

--- src/atlassian-test/links/links.go
+++ src/atlassian-test/links/links.go
@@ -16,9 +16,9 @@ import (
 const FILTER_NAME = "links"
 const TITLE_MAX_LEN = 50
 const MAXHTMLSIZE = 10 * (1 << 10) // 10KiB
-const TIMEOUT = 10                 //10 seconds
+const TIMEOUT = 10                 // 10 seconds
 
-// `(?m)` for multiline mode
+	// `(?m)` for multiline mode
 var filter_re = regexp.MustCompile(`(?m)https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`)
 var title_re = regexp.MustCompile(`(?m)<title>(.*)<\/title>`)
 
@@ -27,27 +27,27 @@ type linksResult struct {
 	Title string `json:"title"`
 }
 
-// Empty struct for filter object
+	// Empty struct for filter object
 type filter struct {
 	get func(string) string
 }
 
-// Filter filters input message and returns founded objects
+	// Filter filters input message and returns founded objects
 func (f *filter) Filter(input string) (output fabric.FilteredResult) {
 	//Looking for matches regarding to filter expression
 	m := filter_re.FindAllString(input, -1)
 	log.WithField("filter", FILTER_NAME).Debugf("Found %d matches", len(m))
 	for _, url := range m {
-		//Adding founded objects
+			//Adding founded objects
 		var res linksResult
 		res.Url = url
-		// Trying to get web page and parse it to find <title>
+			// Trying to get web page and parse it to find <title>
 		tm := title_re.FindAllStringSubmatch(f.get(url), 1)
-		// If successful adding title to the resule
+			// If successful adding title to the resule
 		if len(tm) > 0 {
-			// Unescaping HTML symbols
+				// Unescaping HTML symbols
 			res.Title = html.UnescapeString(tm[0][1])
-			// Cutting to max len
+				// Cutting to max len
 			if len(res.Title) > TITLE_MAX_LEN {
 				res.Title = res.Title[:47] + "..."
 			}
@@ -58,17 +58,19 @@ func (f *filter) Filter(input string) (output fabric.FilteredResult) {
 }
 
 func httpGet(url string) (result string) {
-	// Getting web page with timeout and size limit
+		// Getting web page with timeout and size limit
 	ctx, _ := context.WithTimeout(context.Background(), time.Second*TIMEOUT)
 	hc := new(http.Client)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return
+		
 	}
 	req.WithContext(ctx)
 	r, err := hc.Do(req)
 	if err != nil {
 		return
+		
 	}
 	defer r.Body.Close()
 	buf, _ := ioutil.ReadAll(io.LimitReader(r.Body, MAXHTMLSIZE))
